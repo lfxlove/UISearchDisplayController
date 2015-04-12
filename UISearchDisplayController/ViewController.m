a2eed6d689526332a42ed86de0824a5c97a8bb1c
Mac <852921913@qq.com>
2015-04-12 11:55:59 +0800

    no.1
    maclfx

--- UISearchDisplayController/ViewController.m
+++ UISearchDisplayController/ViewController.m
@@ -38,7 +38,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 10;
+    return 100;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
